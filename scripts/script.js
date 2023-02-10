ecc25dd9f31913fd851fbbecc7914eb24ff59ee9
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-02-10 14:21:04 -0400

    cleaned up code and added comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// Create instance of dayjs object
 const dayjsObj = dayjs();
 
+// init data for the planner entries.
 var plannerEntries = [
   {
     timeSlot: 9,
@@ -43,15 +41,17 @@ var plannerEntries = [
   },
 ];
 
+// Checking if localstorage has any data.
 function getLocalStorageTimeSlots() {
   var retrievedEntries = JSON.parse(localStorage.getItem("plannerEntries"));
   if (retrievedEntries != null) {
     plannerEntries = retrievedEntries;
   }
 }
-
+// run on page load
 getLocalStorageTimeSlots();
 
+// utility function for creating time slot hour labels
 function timeSlotLabel(timeIn24hr) {
   var label;
   if (timeIn24hr > 12) {
@@ -63,6 +63,7 @@ function timeSlotLabel(timeIn24hr) {
   return label;
 }
 
+// utility function for assigning the correct class to the timeslot for styling.
 function isPastPresentOrFuture(timeIn24hr) {
   const currentHour = dayjsObj.format("H");
   if (timeIn24hr < currentHour) {
@@ -74,10 +75,12 @@ function isPastPresentOrFuture(timeIn24hr) {
   }
 }
 
+// Run all dom interactions logic once the page has loaded.
 $(function () {
-  var timeSlots = "";
+  var timeSlotsHtml = "";
+  // create a timeslot entry in html for every item in the plannerEntries array.
   plannerEntries.forEach((entry) => {
-    timeSlots += `
+    timeSlotsHtml += `
     <div id="hour-${
       entry.timeSlot
     }" class="row time-block ${isPastPresentOrFuture(entry.timeSlot)}">
@@ -93,31 +96,15 @@ $(function () {
     </div>
     `;
   });
+  // inject html into the #timeSlots element
+  $("#timeSlots").html(timeSlotsHtml);
 
-  $("#timeSlots").html(timeSlots);
-
+  // Eventlistener for saving text in entry to array then pushing to localstorage
   $(".time-block").click(function () {
     plannerEntries[$(this).index()].text = $(this).find("textarea").val();
     localStorage.setItem("plannerEntries", JSON.stringify(plannerEntries));
   });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Add today's date to the header.
   $("#currentDay").text(dayjsObj.format("MMMM DD, YYYY"));
 });
